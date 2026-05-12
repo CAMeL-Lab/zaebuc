7633e97b1bcfd84514109bc05042376bcdc66218
injy-hamed <injykhairy@gmail.com>
2026-05-12 08:48:25 +0400

    Update word_tokenize_corpus.sh

--- morph/utils/word_tokenize_corpus.sh
+++ morph/utils/word_tokenize_corpus.sh
@@ -1,8 +1,6 @@
 
-dir_public=/Users/injy.hamed/Documents/Projects/Zaebuc/camera_ready/final/release/public
-dir_exp=/Users/injy.hamed/Documents/Projects/Zaebuc/camera_ready/final/release/experiments
-#dir_public=/scratch/ba63/zaebuc-lrec-2026/public-release
-#dir_exp=/scratch/ba63/zaebuc-lrec-2026/expriments/
+dir_public=/scratch/ba63/zaebuc-lrec-2026/public-release
+dir_exp=/scratch/ba63/zaebuc-lrec-2026/expriments/
 
 python word_tokenize_corpus.py \
     --input_transcriptions ${dir_public}/spoken/zaebuc-s/transcriptions/text \
