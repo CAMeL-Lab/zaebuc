b21c3932e1ecccb8575486f15bcdcd0287520832
injy-hamed <injykhairy@gmail.com>
2026-05-12 08:48:13 +0400

    Update process_llm_annotation_files.sh

--- morph/utils/process_llm_annotation_files.sh
+++ morph/utils/process_llm_annotation_files.sh
@@ -1,8 +1,6 @@
 
-dir_public=/Users/injy.hamed/Documents/Projects/Zaebuc/camera_ready/final/release/public
-dir_exp=/Users/injy.hamed/Documents/Projects/Zaebuc/camera_ready/final/release/experiments
-#dir_public=/scratch/ba63/zaebuc-lrec-2026/public-release
-#dir_exp=/scratch/ba63/zaebuc-lrec-2026/expriments/
+dir_public=/scratch/ba63/zaebuc-lrec-2026/public-release
+dir_exp=/scratch/ba63/zaebuc-lrec-2026/expriments/
 
 for model in gpt-4o gpt-5; do
   python process_llm_annotation_files.py \
