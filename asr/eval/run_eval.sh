dada618bcd8926b04e1c9028ddb3edfdf2e48063
injy-hamed <injykhairy@gmail.com>
2026-05-12 08:55:26 +0400

    Update run_eval.sh

--- asr/eval/run_eval.sh
+++ asr/eval/run_eval.sh
@@ -1,9 +1,7 @@
 # Reporting WER for multichannel (combined) and single channel (separate)
 
-dir_public=/Users/injy.hamed/Documents/Projects/Zaebuc/camera_ready/final/release/public
-dir_exp=/Users/injy.hamed/Documents/Projects/Zaebuc/camera_ready/final/release/experiments
-#dir_public=/scratch/ba63/zaebuc-lrec-2026/public-release
-#dir_exp=/scratch/ba63/zaebuc-lrec-2026/expriments/
+dir_public=/scratch/ba63/zaebuc-lrec-2026/public-release
+dir_exp=/scratch/ba63/zaebuc-lrec-2026/expriments/
 
 #Without Language Flag
 for channel in multichannel singlechannel; do
