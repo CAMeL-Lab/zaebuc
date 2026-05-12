e9967e417c0948f30ef63cbdad3101bbe8c23a2a
injy-hamed <injykhairy@gmail.com>
2026-05-12 08:44:42 +0400

    Update update_morph_annotations.sh

--- morph/morph_tagging/spoken/update_morph_annotations.sh
+++ morph/morph_tagging/spoken/update_morph_annotations.sh
@@ -1,9 +1,9 @@
-dir=/Users/injy.hamed/Documents/Projects/Zaebuc/camera_ready/final/release
-#dir=/scratch/ba63/zaebuc-lrec-2026
+dir_public=/scratch/ba63/zaebuc-lrec-2026/public-release
+dir_exp=/scratch/ba63/zaebuc-lrec-2026/expriments/
 
 python update_morph_annotations.py \
-    --input_transcriptions ${dir}/experiments/spoken/morph/data/text_tok.all.txt \
-    --prev_morph_annotations ${dir}/public/spoken/morph/previous_morph_annotations \
-    --output ${dir}/experiments/spoken/morph/models/camelTools+stanza/all.tsv
+    --input_transcriptions ${dir_public}/spoken/zaebuc-s/morph/all.transcriptions.tok \
+    --prev_morph_annotations ${dir_exp}/spoken/morph/data/previous_morph_annotations \
+    --output ${dir_exp}/spoken/morph/models/camelTools+stanza/all.input_redacted.morph.tsv
 
   
