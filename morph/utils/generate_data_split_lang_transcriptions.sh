e0e13cde17e47ec090830118e47a84e55f481409
injy-hamed <injykhairy@gmail.com>
2026-05-12 08:47:58 +0400

    Update generate_data_split_lang_transcriptions.sh

--- morph/utils/generate_data_split_lang_transcriptions.sh
+++ morph/utils/generate_data_split_lang_transcriptions.sh
@@ -1,8 +1,6 @@
 
-dir_public=/Users/injy.hamed/Documents/Projects/Zaebuc/camera_ready/final/release/public
-dir_exp=/Users/injy.hamed/Documents/Projects/Zaebuc/camera_ready/final/release/experiments
-#dir_public=/scratch/ba63/zaebuc-lrec-2026/public-release
-#dir_exp=/scratch/ba63/zaebuc-lrec-2026/expriments/
+dir_public=/scratch/ba63/zaebuc-lrec-2026/public-release
+dir_exp=/scratch/ba63/zaebuc-lrec-2026/expriments/
 
 #AR tokenized transcriptions
 python generate_data_split_lang_transcriptions.py \
